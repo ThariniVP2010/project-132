baca3417d915cd9a237bcc710fc77e45c4b71140
ThariniVP2010 <97461670+ThariniVP2010@users.noreply.github.com>
2022-01-13 17:09:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting";
+}
+
+function modelLoaded() {
+    console.log("model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
